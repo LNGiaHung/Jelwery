75dcc4f5a24366a688db677fb88b884188494518
ditconhanhphuc <21522021@gm.uit.edu.vn>
2024-06-01 21:42:50 +0700

    update role admin

--- Bản sao Bijou/roleAdmin/statistic.js
+++ Bản sao Bijou/roleAdmin/statistic.js
@@ -255,3 +255,138 @@ function UpdateAreaChart(invoiceData) {
   const areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
   areaChart.render();
 }
+
+
+// -------------------------Order Management ------------------------------
+document.getElementById('editModeBtn').addEventListener('click', function(e) {
+  e.preventDefault();
+  const table = document.querySelector('.recentOrders table');
+  table.classList.toggle('edit-mode');
+  this.textContent = table.classList.contains('edit-mode') ? 'Save' : 'Edit Mode';
+});
+
+
+// Area chart for quantity and earning on 12 months
+// const areaChartEarningOptions = {
+//   series: [
+//     {
+//       name: 'Sale Orders (Done)',
+//       data: doneCounts,
+//     },
+//     {
+//       name: 'Purchase Orders (All)',
+//       data: All,
+//     },
+//   ],
+//   chart: {
+//     height: 350,
+//     type: 'area',
+//     toolbar: {
+//       show: false,
+//     },
+//   },
+//   colors: ["#4f35a1", "#246dec"],
+//   dataLabels: {
+//     enabled: false,
+//   },
+//   stroke: {
+//     curve: 'smooth',
+//   },
+//   labels: months,
+//   markers: {
+//     size: 0,
+//   },
+//   yaxis: [
+//     {
+//       title: {
+//         text: 'Earning',
+//       },
+//     },
+//     {
+//       opposite: true,
+//       title: {
+//         text: 'Quantity',
+//       },
+//     },
+//   ],
+//   tooltip: {
+//     shared: true,
+//     intersect: false,
+//   },
+// };
+// const areaChartEarning = new ApexCharts(document.querySelector("#area-chart_earning-quantity"), areaChartEarningOptions);
+// areaChartEarning.render();
+
+var groupChartEarningOptions = {
+  series: [
+  {
+    name: 'Last Year',
+    group: 'budget',
+    data: [44000, 55000, 41000, 67000, 22000, 43000]
+  },
+  {
+    name: 'This Year',
+    group: 'actual',
+    data: [64000, 35000, 46000, 60000, 62000, 40000]
+  }
+],
+  chart: {
+  type: 'bar',
+  height: 550,
+  stacked: true,
+},
+stroke: {
+  width: 1,
+  colors: ['#fff']
+},
+dataLabels: {
+  formatter: (val) => {
+    return val / 1000 + 'K'
+  }
+},
+plotOptions: {
+  bar: {
+    horizontal: false
+  }
+},
+xaxis: {
+  title: {
+    text: 'Month'
+  },
+  categories: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    // 'July'
+    // 'August',
+    // 'September',
+    // 'October',
+    // 'November',
+    // 'December'
+  ]
+},
+fill: {
+  opacity: 1
+},
+colors: ['#80c7fd', '#00E396'],
+yaxis: {
+  title: {
+    text: 'Revenue'
+  },
+  labels: {
+    formatter: (val) => {
+      return val / 1000 + 'K'
+    }
+  }
+},
+legend: {
+  position: 'top',
+  horizontalAlign: 'left'
+}
+};
+
+var groupChartEarning = new ApexCharts(document.querySelector("#group-chart_earning-quantity"), groupChartEarningOptions);
+groupChartEarning.render();
